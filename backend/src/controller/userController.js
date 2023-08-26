ecba54ce1c37d5d5b38f8a0183bdf756ead9bcc3
jasmeendesai <130737953+jasmeendesai@users.noreply.github.com>
2023-08-26 12:50:44 +0530

    Update userController.js

--- backend/src/controller/userController.js
+++ backend/src/controller/userController.js
@@ -1,12 +1,3 @@
-// "scripts": {
-//     "start": "concurrently \"npm run server\" \"npm run client\"",
-//     "server": "node index.js",
-//     "client": "cd backend && npx nodemon src/index.js"
-//   }
-//     "start": "react-scripts start",
-  // "both" : "concurrently both \"npm run start\" \"npx nodemon backend/src/index.js\""
-
-
 const UserModel = require("../model/UserModel");
 const {
   isValid,
@@ -99,10 +90,15 @@ const login = async (req, res) => {
     }
     //find user using emailId
     const user = await UserModel.findOne({ email: email });
+    if(!user) {
+      return res
+      .status(400)
+      .send({ status: false, message: "Enter the valid user credentials" }); 
+     }
 
     const passwordStatus = await comparePassword(password, user.password)
 
-    if(!user || !passwordStatus) {
+    if(!passwordStatus) {
         return res
         .status(400)
         .send({ status: false, message: "Enter the valid user credentials" }); 
